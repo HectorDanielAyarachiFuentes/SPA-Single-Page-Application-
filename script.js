f18f3e1bf01ce4d7cac44d5b7a1d6ff276ae6d59
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-10-02 13:43:04 -0300

    ..

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-function cargarPagina(pagina) {
-      var contenido = document.getElementById('content');
-      if (pagina === 'aviones') {
-        contenido.innerHTML = '<h1>Aviones</h1><p>Esta es la sección de aviones.</p><img src="https://www.preferente.com/wp-content/uploads/2023/04/copa.jpg" alt="Avión" class="contenido-img">';
-        contenido.className = 'aviones';
-      } else if (pagina === 'tanques') {
-        contenido.innerHTML = '<h1>Tanques</h1><p>Esta es la sección de tanques.</p><img src="https://www.publico.es/files/article_main//files/crop/uploads/2020/04/20/5e9dafd247003.r_1587416005789.143-0-1186-695.jpeg" alt="Tanque" class="contenido-img">';
-        contenido.className = 'tanques';
-      } else if (pagina === 'Barcos') {
-        contenido.innerHTML = '<h1>Barcos</h1><p>Esta es la sección de barcos.</p><img src="https://i0.wp.com/ensedeciencia.com/wp-content/uploads/2023/01/Copia-de-C-EdCiencia-P-2023-01-24T103519.000.jpg?resize=1200%2C676&ssl=1" alt="Barco" class="contenido-img">';
-        contenido.className = 'barcos';
-      } else if (pagina === 'Helicopteros') {
-        contenido.innerHTML = '<h1>Helicópteros</h1><p>Esta es la sección de helicópteros.</p><img src="https://israelnoticias.com/wp-content/uploads/2023/04/HELICOPTERO_FUTURO-750x536.webp" alt="Helicóptero" class="contenido-img">';
-        contenido.className = 'helicopteros';
-      } else if (pagina === 'Armas') {
-        contenido.innerHTML = '<h1>Armas</h1><p>Esta es la sección de armas.</p><img src="https://cdnuploads.aa.com.tr/uploads/Contents/2022/04/12/thumbs_b_c_c8ccfd7c3f0c7f83c7627535f0118af8.jpg?v=010516" alt="Arma" class="contenido-img">';
-        contenido.className = 'armas';
-      }
-    }
+// Obtener los datos desde el archivo JSON "datos.json"
+fetch('datos.json')
+  .then(response => response.json())
+  .then(data => {
+    // Los datos se han cargado correctamente
+    // Ahora puedes trabajar con los datos
+
+    // Agregar un evento de clic a cada enlace de navegación
+    const enlaces = document.querySelectorAll('nav ul li a');
+    enlaces.forEach(enlace => {
+      enlace.addEventListener('click', function (event) {
+        event.preventDefault();
+        const pagina = this.getAttribute('data-pagina');
+        cargarPagina(pagina, data);
+      });
+    });
 
     // Cargar la página de aviones por defecto
-    cargarPagina('aviones');
+    cargarPagina('aviones', data);
+  })
+  .catch(error => {
+    console.error('Error al cargar el archivo JSON:', error);
+  });
+
+function cargarPagina(pagina, data) {
+  var contenido = document.getElementById('content');
+  var seccion = obtenerSeccionPorID(pagina, data);
+  if (seccion) {
+    contenido.innerHTML = `<h1>${seccion.titulo}</h1><p>${seccion.descripcion}</p><img src="${seccion.imagen}" alt="${seccion.titulo}" class="contenido-img">`;
+    contenido.className = seccion.id;
+  }
+}
+
+function obtenerSeccionPorID(id, data) {
+  // Obtener la sección correspondiente del JSON por su ID
+  return data.find(seccion => seccion.id === id);
+}
